7e77fbb679c58dc2a983e9bf3fe927ccf22b9c15
himarnoel <olaimarnoel>
2024-03-31 23:43:41 +0100

    working

--- payment/urls.py
+++ payment/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
 from .views import PaymentView
 urlpatterns = [
-      path('pay',PaymentView.as_view()),
+      path('payment',PaymentView.as_view(), name='payment-record'),
 ]
--- payment/views.py
+++ payment/views.py
@@ -6,13 +6,54 @@ from .models import Payment
 from rest_framework.response import Response
 from .serializers import PaymentSerializer
 from rest_framework.views import APIView
+from core.serializers import JobSerializer
 from rest_framework.permissions import IsAuthenticated, IsAdminUser
 from rest_framework.generics import RetrieveAPIView, ListAPIView,ListCreateAPIView,CreateAPIView
 
 
 # Create your views here.
 class PaymentView(APIView):
-    permission_classes = [IsAuthenticated] 
+    permission_classes = [IsAuthenticated]
+    def post(self, request):
+        # Check if the transaction exists
+        # try:
+        #     transaction = PaymentSerializer.objects.get(paystack_reference=reference)
+        #     return Response({'message': 'Transaction already processed'}, status=status.HTTP_200_OK)
+        # except PaymentSerializer.DoesNotExist:
+        #     pass
+        
+        # transaction_data = {
+        #     'paystack_reference': reference,
+        #     'amount':amount, # get amount from Paystack or frontend,
+        #     'status': 'successful',
+        #     # Add any other transaction details
+        #     }
+        
+        payment_data = {
+            'paystack_reference': request.data.get('paystack_reference'),
+            'amount':request.data.get('amount'), # get amount from Paystack or frontend,
+            'status': 'successful',
+            # Add any other transaction details
+            }
+
+        job_data= {
+            'job_type':request.data.get('job_type'),
+            'city':request.data.get('city'), # get amount from Paystack or frontend,
+            'street_adddress':request.data.get('street_adddress'),
+            "job_description":request.data.get('job_description'),
+            "time":request.data.get('time'),
+            "completed":request.data.get('completed'),
+            # Add any other transaction details
+            }
+        jobSerializer=JobSerializer(data=job_data, context={"userinfo":request.user})
+        payment_serializer = PaymentSerializer(data=payment_data, context={"userinfo":request.user})
+        if payment_serializer.is_valid() and  jobSerializer.is_valid():
+            payment_serializer.save()
+            jobSerializer.save()
+            return Response({'message': 'Transaction recorded successfully'}, status=status.HTTP_201_CREATED)
+        else:
+            return Response(payment_serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+     
     def get(self, request):
         payments = Payment.objects.all()
         serializer = PaymentSerializer(payments, many=True)
